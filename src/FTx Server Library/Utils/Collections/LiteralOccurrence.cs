21d1e2b93fd3b84a34517c08acadfc83576fb228
qodfathr <todd_mancini@hotmail.com>
2016-07-20 11:54:23 -0400

    added copyright info

--- src/FTx Server Library/Utils/Collections/LiteralOccurrence.cs
+++ src/FTx Server Library/Utils/Collections/LiteralOccurrence.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2002-2016 Daxat, Inc. and Todd A. Mancini. All rights reserved.
+// Daxat, Inc. and Todd A. Mancini licenses this file to you under the MIT license. 
+// See the LICENSE file in the project root for more information. 
 using System;
 
 namespace Daxat.Ess.Utils.Collections
